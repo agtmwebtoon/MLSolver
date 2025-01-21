3843bdb39cff4550ef468c20bc33a5f516b7c377
Minhyung Lee <ss49132002@gmail.com>
2025-01-21 16:38:07 +0900

    DEALii solver heavily follow Jean-Paul Pelteret and Andrew McBride

---
+++ FEM.h
@@ -0,0 +1,59 @@
+//
+//  FEM.h
+//  main
+//
+//  Created by 이민형 on 1/21/25.
+//
+
+#ifndef FEM_h
+#define FEM_h
+
+#include <deal.II/base/function.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/point.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/symmetric_tensor.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/base/timer.h>
+#include <deal.II/base/work_stream.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/base/quadrature_point_data.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/fe/fe_dgp.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_tools.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q_eulerian.h>
+
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/block_sparse_matrix.h>
+#include <deal.II/lac/block_vector.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition_selector.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_selector.h>
+#include <deal.II/lac/sparse_direct.h>
+
+#include <deal.II/lac/linear_operator.h>
+#include <deal.II/lac/packaged_operation.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <deal.II/physics/elasticity/kinematics.h>
+#include <deal.II/physics/elasticity/standard_tensors.h>
+
+#include <fstream>
+#include <iostream>
+
+
+#endif /* FEM_h */
---
+++ utill/Parameters.h
@@ -0,0 +1,87 @@
+//
+//  Parameters.h
+//  main
+//
+//  Created by 이민형 on 1/21/25.
+//
+
+#ifndef Parameters_h
+#define Parameters_h
+
+#include <deal.II/base/parameter_handler.h>
+#include <string>
+
+namespace MLSolver {
+using namespace dealii;
+
+namespace Parameters {
+
+struct FESystem {
+    unsigned int poly_degree;
+    unsigned int quad_order;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct Geometry {
+    unsigned int global_refinement;
+    double scale;
+    double p_p0;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct Materials {
+    double nu;
+    double mu;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct LinearSolver {
+    std::string type_lin;
+    double tol_lin;
+    double max_iterations_lin;
+    bool use_static_condensation;
+    std::string preconditioner_type;
+    double preconditioner_relaxation;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct NonlinearSolver {
+    unsigned int max_iterations_NR;
+    double tol_f;
+    double tol_u;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct Time {
+    double delta_t;
+    double end_time;
+
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+struct AllParameters : public FESystem,
+                       public Geometry,
+                       public Materials,
+                       public LinearSolver,
+                       public NonlinearSolver,
+                       public Time {
+    AllParameters(const std::string &input_file);
+    static void declare_parameters(ParameterHandler &prm);
+    void parse_parameters(ParameterHandler &prm);
+};
+
+} // namespace Parameters
+} // namespace MLSolver
+
+#endif /* Parameters_h */
